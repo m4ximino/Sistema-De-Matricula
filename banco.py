5ef08d8a10f7ee0a991d3b584671efc78d6016a7
Mayco Maximino <mmaa@ic.ufal.br>
2022-08-20 23:05:33 -0300

    Alterações finais

--- banco.py
+++ banco.py
@@ -5,7 +5,7 @@ from mysql.connector import Error
 # Inserts no banco de dados
 def inscricao_aluno(nome, coeficiente, prioridade, semestre):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
         cursor = con.cursor()
         inscricao = (f"INSERT INTO aluno (nome, coeficiente, prioridade, semestre) VALUES ('{nome}', '{coeficiente}', '{prioridade}', '{semestre}');")
         cursor.execute(inscricao)
@@ -19,7 +19,7 @@ def inscricao_aluno(nome, coeficiente, prioridade, semestre):
 
 def inscricao_materia(aluno: int,materia: int):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
         cursor = con.cursor()
         inscricao = (f"INSERT INTO materias_inscritas (id_aluno, id_materias_cursadas) VALUES ({aluno}, {materia});")
         cursor.execute(inscricao)
@@ -34,7 +34,7 @@ def inscricao_materia(aluno: int,materia: int):
 
 def materias_cursadas(aluno,materia):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
         cursor = con.cursor()
         inscricao = (f"INSERT INTO materias_cursadas (id_aluno, id_materias_cursadas) VALUES ({aluno}, {materia});")
         cursor.execute(inscricao)
@@ -50,7 +50,7 @@ def materias_cursadas(aluno,materia):
 # Pegar valores banco de dados
 def pegar_valores_materias_inscritas(aluno):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
 
         consulta_sql = f"SELECT * FROM materias_inscritas WHERE id_aluno = {aluno};"
         cursor = con.cursor()
@@ -73,7 +73,7 @@ def pegar_valores_materias_inscritas(aluno):
 
 def pegar_valores_pre_requesitos(id):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
 
         consulta_sql = f"SELECT * FROM pre_requisitos WHERE id_materias_ = {id};"
         cursor = con.cursor()
@@ -94,7 +94,7 @@ def pegar_valores_pre_requesitos(id):
 
 def pegar_valores_materias_cursadas(aluno):
     try:
-        con = mysql.connector.connect(host='127.0.0.1', database='mydb', user='newuser', password='Whinttemore456@')
+        con = mysql.connector.connect(host='', database='', user='', password='')
 
         consulta_sql = f"SELECT * FROM materias_cursadas WHERE id_aluno = {aluno};"
         cursor = con.cursor()
